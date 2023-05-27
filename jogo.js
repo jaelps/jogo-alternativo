8b5db46e81e4f4a63e6b9296f7b9d14cfde1dc7b
jaelps <jaelsonpsilva18@gmail.com>
2023-05-27 08:59:38 -0300

    alteração para iniciar interação de ainda não func

--- jogo.js
+++ jogo.js
@@ -1,12 +1,19 @@
-resp = window.document.getElementById('x')
+var resposta = ''
+var ativadorS = window.document.querySelector('input.bt-s')
+var ativadorN = window.document.querySelector('input.bt-n')
+var boxP = window.document.querySelector('.areaPrincipal')
+var resposta = sValor
+var resposta = nValor
 
 
-function interacao(){
-    r = String(r.value)
-    if (r == 'oi'){
-        resp.innerHTML += 'Onde eu estou ?'
-    }
+function animacaoDeCarregamento(){
+  var sValor = ativadorS.value
+  var nValor = ativadorN.value
+  if (resposta == 'sim'){
+      boxP.innerHTML = '<p>Iniciando conexão ...</p>'
+  }
+    /*boxP.innerHTML = '<p>Iniciando conexão ...</p>'*/
 }
 
-
-resp.addEventListener('keypress', interacao)
+ativadorS.addEventListener('click', animacaoDeCarregamento())
+ativadorN.addEventListener('click', animacaoDeCarregamento())
